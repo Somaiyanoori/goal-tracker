8911be85abd0e29a5f58c1d4bba8a1bfda7c6a3e
Somaiya Noori <164001610+Somaiyanoori@users.noreply.github.com>
2026-03-17 11:54:39 +0430

    Fix import paths for localization files
    Updated import paths for language JSON files.

--- src/i18n.js
+++ src/i18n.js
@@ -1,8 +1,8 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import en from "./locals/en.json";
-import fa from "./locals/fa.json";
+import en from "./locales/en.json";
+import fa from "./locales/fa.json";
 i18n
   .use(LanguageDetector) 
   .use(initReactI18next) 
